1d2bbf24a2d61cdd24896f83d84655a3eb8cdc88
Enrique W <kique10000@gmail.com>
2013-08-04 22:02:00 -0400

    initial commit

---
+++ app/controllers/kique_login_controller.rb
@@ -0,0 +1,24 @@
+class KiqueLoginController < ApplicationController
+  before_filter :require_user, :only => [:about, :contactus]
+
+  def index
+  end
+
+  def about
+  end
+
+  def contactus
+  end
+  
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KiqueLogin::Application.config.secret_key_base = 'ae69f89d1b22ff97b4612886dbb49820b0773703a0647396315d63dec3a74f9ff7b7287702239294b724e78b21e2a3ebbe7c094eb314ad3a6a2f350340fc2115'
